7315564a0e465366b7441387ed0a53d2ba5b8cdb
felipetacara <felipetacara@gmail.com>
2025-06-05 15:08:02 -0300

    implement sentry, companion components , filter and subcriptions

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,31 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import Cta from '@/components/CTA'
-import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 })
+  const recentSessionsCompanions = await getRecentSessions(10)
+
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the brain"
-          subject="science"
-          duration={45}
-          color="#ff00ff"
-        />
-        <CompanionCard
-          id="456"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the brain"
-          subject="science"
-          duration={45}
-          color="#ff00ff"
-        />
-        <CompanionCard
-          id="678"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the brain"
-          subject="science"
-          duration={45}
-          color="#ff00ff"
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+
 
       </section>
       <section className="home-section">
         <CompanionsList
           title="Recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <Cta />
---
+++ instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d949b97755e130e87910b9f56d44cf07@o4509411991027712.ingest.us.sentry.io/4509447256866816",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
